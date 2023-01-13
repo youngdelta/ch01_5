89311f6c0d78d3f5b7cfdaf25e55bc3bfd50716e
_ <youngdelta@gmail.com>
2023-01-13 18:54:23 +0900

    ts-react

--- src/data/image.ts
+++ src/data/image.ts
@@ -2,4 +2,10 @@ import * as U from './util';
 //prettier-ignore
 export const picsumUrl=(width:number, height:number):string => `https://picsum.com/${width}x${height}`;
 
-export const randomImage=(width:number=1000, height:number=800,delta:number=200):string => picsumUrl(U.random(width,width+delta), U.random(height,height+delta)
+export const randomImage = (width: number = 1000, height: number = 800, delta: number = 200): string =>
+	picsumUrl(U.random(width, width + delta), U.random(height, height + delta));
+
+export const randomAvatar = () => {
+	const size = U.random(200, 400);
+	return picsumUrl(size, size);
+};
